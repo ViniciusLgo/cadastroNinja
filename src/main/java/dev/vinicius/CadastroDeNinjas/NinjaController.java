af994a8e9b7d015102b2379f6a0467fdcbc0351f
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-02-04 13:57:07 -0300

    Criando Entidade no banco de dados a partir ddo NINJAMODEL - aula 03 -

---
+++ src/main/java/dev/vinicius/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,19 @@
+package dev.vinicius.CadastroDeNinjas; // Define o pacote onde a classe está localizada.
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Anotação que indica que essa classe é um controlador REST no Spring Boot.
+@RestController
+// Define o mapeamento base para as requisições HTTP.
+@RequestMapping
+public class NinjaController { // Declaração da classe Controller.
+
+    // Mapeia requisições HTTP GET para o endpoint "/boasvindas".
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        // Retorna uma mensagem de boas-vindas como resposta à requisição.
+        return "essa é minha primeira mensaagem nessa roda boas vindo";
+    }
+}
