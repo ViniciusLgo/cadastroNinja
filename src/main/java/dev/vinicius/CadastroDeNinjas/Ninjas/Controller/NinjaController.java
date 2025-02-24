12584e7d2622086449b33b22a4b7841871b6cc08
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-02-23 22:36:09 -0300

    feat: Mock de endpoint conf NinjaController

--- src/main/java/dev/vinicius/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
+++ src/main/java/dev/vinicius/CadastroDeNinjas/Ninjas/Controller/NinjaController.java
@@ -1,7 +1,5 @@
 package dev.vinicius.CadastroDeNinjas.Ninjas.Controller; // Define o pacote onde a classe está localizada.
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // Anotação que indica que essa classe é um controlador REST no Spring Boot.
 @RestController
@@ -15,5 +13,37 @@ public class NinjaController { // Declaração da classe Controller.
         // Retorna uma mensagem de boas-vindas como resposta à requisição.
         return "essa é minha primeira mensaagem nessa roda boas vindo";
     }
+
+    // add ninja(create)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "ninja criado";
+    }
+
+    // procurar por id (create )
+
+    // mostrar os ninjas  ( read)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostar Ninjas";
+    }
+// mostrar os ninjas por ID  ( read)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorId(){
+        return "Mostar Ninjas por ID";
+    }
+
+    //alterar dados dos ninjas(update)
+    @PutMapping("/alterarID")
+    public String alterarNinjaId(){
+        return "Alterar Ninjas por ID";
+    }
+
+    // deletar ninja (delete)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaId(){
+        return "Deletar Ninjas por ID";
+    }
+
 }
 
