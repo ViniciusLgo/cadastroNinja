3db6b8e911016e48ff4b306001dc25fc970de70d
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-03-15 12:42:59 -0300

    feat: criar as rotas do Controller com mock

--- src/main/java/dev/vinicius/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/vinicius/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,33 @@
 package dev.vinicius.CadastroDeNinjas.Missoes;
 
-public class missoesController {
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
+public class MissoesController {
+
+    // Get - requisao para mostrar missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return "missoes listadas com sucesso";
+    }
+
+    // Post - requisao para criar missoes
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "missao criada com sucesso";
+    }
+
+    // Put - requisao para alterar missoes
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "missao alterada com sucesso";
+    }
+
+    // delete - requisao para deletar missoes
+    @DeleteMapping("/delete")
+    public String deletarMissao(){
+        return "missao deletada com sucesso";
+    }
+
 }
