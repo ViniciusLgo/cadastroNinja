0b6649cd51c2823c33ef783e23cf604fb1539edb
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-04-06 15:29:04 -0300

    chore: correção de estrutura e comentário no main da aplicação

--- src/main/java/dev/vinicius/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/vinicius/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -3,11 +3,13 @@ package dev.vinicius.CadastroDeNinjas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal que inicia a aplicação Spring Boot.
+ */
 @SpringBootApplication
-public class 	CadastroDeNinjasApplication {
+public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
-
 }
