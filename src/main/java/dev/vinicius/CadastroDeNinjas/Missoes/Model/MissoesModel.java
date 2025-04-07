81b08288a7840a4bb150b0af77aaadb3355fa8d6
Vinicius Lago Cruz <vinicruzlago@gmail.com>
2025-04-06 21:00:18 -0300

    refactor: ajustes na entidade MissoesModel para melhor integração com UI

--- src/main/java/dev/vinicius/CadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/dev/vinicius/CadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class MissoesModel {
 
     /** Chave primária da missão */
